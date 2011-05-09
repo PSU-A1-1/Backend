12f7a6256507d3c8ef572b3faead7620e6308933
Kasper <kasper@kasperhelweg.dk>
2011-05-09 13:28:42 +0200

    Menu

--- connect.php
+++ connect.php
@@ -1,8 +1,10 @@
-<?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'stengade';
+<?PHP 
+$dbhost = 'kasperhelweg.dk.mysql';
+$dbuser = 'kasperhelweg_dk';
+$dbpass = 'sql2211';
+$dbname = 'kasperhelweg_dk';
+
 mysql_connect($dbhost, $dbuser, $dbpass) or die ('MYSQL connection error');
 mysql_select_db($dbname);
+
 ?>
