10d3f43ba5037209d3d7da31489b3a28abf9280b
Kasper <kasper@kasperhelweg.dk>
2011-05-11 13:45:38 +0200

    still working on admin stuff

--- View/new_volunteer_view.php
+++ View/new_volunteer_view.php
@@ -5,6 +5,7 @@ $().ready(
 		{
 			$("#volunteerAdd").click(function() 
 			{	
+				alert($('#aktiv').val()); 
 			if ($('#volunteerName').val() != "") {
 				alert("cool");
 			} else {
@@ -23,5 +24,7 @@ $().ready(
 <form action="" method="post" >
 Tilføj ny frivillig<br> 
 <input type="text" name="name" id="volunteerName" /> <input type='hidden' id='volunteerName' />
+<input type="text" name="name" id="volunteerSurName" /> <input type='hidden' id='volunteerSurName' />
+<input type="checkbox" name="aktiv" value="aktiv" /> Aktiv<br />
 <input type="button" value="OK" id="volunteerAdd">
 </form> 
