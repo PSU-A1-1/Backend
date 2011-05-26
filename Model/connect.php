6f1113ef91f8250f0203e7d119fcf1221aebd9ce
Kasper <kasper@kasperhelweg.dk>
2011-05-26 12:11:54 +0200

    damn connect file

--- Model/connect.php
+++ Model/connect.php
@@ -1,8 +1,8 @@
 <?PHP 
-$dbhost = 'kasperhelweg.dk.mysql';
-$dbuser = 'kasperhelweg_dk';
-$dbpass = 'sql2211';
-$dbname = 'kasperhelweg_dk';
+$dbhost = '';
+$dbuser = '';
+$dbpass = '';
+$dbname = '';
 
 mysql_connect($dbhost, $dbuser, $dbpass) or die ('MYSQL connection error');
 mysql_select_db($dbname);
